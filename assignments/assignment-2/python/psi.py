cc2ef3cbfb8858aef37b1a13ca364cbf02c7191f
mammothb <mammothb+git@gmail.com>
2019-10-22 11:23:36 +0800

    used dictionary to implement switch

--- assignments/assignment-2/python/psi.py
+++ assignments/assignment-2/python/psi.py
@@ -1,36 +1,21 @@
 def psi(num, der, xi_1, xi_2):
-    if der == 0:
-        if num == 0:
-            return (1 - xi_1) * (1 - xi_2)
-        elif num == 1:
-            return xi_1 * (1 - xi_2)
-        elif num == 2:
-            return (1 - xi_1) * xi_2
-        elif num == 3:
-            return xi_1 * xi_2
-        else:
-            return 0
-    elif der == 1:
-        if num == 0:
-            return -1 * (1 - xi_2)
-        elif num == 1:
-            return 1 - xi_2
-        elif num == 2:
-            return -xi_2
-        elif num == 3:
-            return xi_2
-        else:
-            return 0
-    elif der == 2:
-        if num == 0:
-            return -1 * (1 - xi_1)
-        elif num == 1:
-            return -xi_1
-        elif num == 2:
-            return 1 - xi_1
-        elif num == 3:
-            return xi_1
-        else:
-            return 0
-    else:
-        return 0
+    return {
+        0: {
+            0: (1 - xi_1) * (1 - xi_2),
+            1: xi_1 * (1 - xi_2),
+            2: (1 - xi_1) * xi_2,
+            3: xi_1 * xi_2
+        },
+        1: {
+            0: -(1 - xi_2),
+            1: 1 - xi_2,
+            2: -xi_2,
+            3: xi_2
+        },
+        2: {
+            0: -(1 - xi_1),
+            1: -xi_1,
+            2: 1 - xi_1,
+            3: xi_1
+        }
+    }.get(der, 0).get(num, 0)
